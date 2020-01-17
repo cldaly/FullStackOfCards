31c8c8341b1aa56074e606e81e7935ad2140614d
cldaly <clayton.daly91@gmail.com>
2020-01-17 09:50:19 -0500

    initial commit

---
+++ src/main/java/com/cards/fullstack/FullStackOfCardsApplication.java
@@ -0,0 +1,13 @@
+package com.cards.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackOfCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackOfCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cards/fullstack/FullStackOfCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cards.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackOfCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
