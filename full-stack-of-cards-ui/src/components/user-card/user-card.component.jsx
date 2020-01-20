bdfa79bb79493673e5e5f54b8abb7946da90aa52
Unknown <will3shu@gmail.com>
2020-01-20 12:31:49 -0500

    UserCardPage Edit, Save, and Remve

--- full-stack-of-cards-ui/src/components/user-card/user-card.component.jsx
+++ full-stack-of-cards-ui/src/components/user-card/user-card.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../flash-card/flash-card.styles.css';
+import Axios from 'axios'
+import {Card} from '../../models/Card'
+import './user-card.styles.css'
 
 class UserCard extends React.Component {
 
     constructor({flashCard}) {
         super()
         this.state = {
-            flashCard : flashCard,
+            questionError : false,
+            answerError : false,
+            flashCard : flashCard,            
             editing: false
         }
     }
@@ -16,7 +21,37 @@ class UserCard extends React.Component {
         this.setState({editing : true}) 
     }
 
+    Remove = () => {
+        Axios.delete('http://localhost:8080/flashcards/removeCard/'+ this.state.flashCard.id)
+    }
+
     Save = () => {
+        const question = document.getElementById('eQuestion').value
+        const answer = document.getElementById('eAnswer').value
+        const link = document.getElementById('eResLink').value
+        this.setState({questionError : false})
+        this.setState({answerError : false})
+
+        //  Prompt error if question or answer is empty
+        if(question === "" || answer === "")
+        {
+            if(question === '')
+                this.setState({questionError : true})
+            if(answer === '')
+                this.setState({answerError : true})             
+            return
+        }        
+
+        //  Create a new card to update on database and page
+        var newCard = new Card()
+        newCard.question = question
+        newCard.answer = answer
+        newCard.resourceLink = link
+        newCard.id = this.state.flashCard.id
+        
+        Axios.put('http://localhost:8080/flashcards/updateCard', newCard)
+        this.setState({flashCard : newCard})
+
         this.setState({editing : false})
     }
 
@@ -30,26 +65,41 @@ class UserCard extends React.Component {
                             <div className="card-details">
                                 <p>A: {this.state.flashCard.answer}</p>
                                 <div className="card-actions">
-                                    <a className='reference' target="_blank" rel='noreferrer noopener' href={this.state.flashCard.resourceLink}>{this.state.flashCard.resourceName}</a>
+                                    <a className='reference' target="_blank" rel='noreferrer noopener' href={this.state.flashCard.resourceLink}>More Info</a>
                                 </div>                    
                             </div> 
-                            <button onClick={this.Edit}>Edit</button>
+                            <button id='new-btn' onClick={this.Edit}>Edit</button>
+                            <button id='new-btn' onClick={this.Remove}>Remove</button>
                         </div>                        
                     }
                     {this.state.editing && 
-                        <div>
-                            <h3>Q: <input type="text" value={this.state.flashCard.question} /></h3>            
-                            <div className="card-details">
-                                <p>A: <input type="text" value={this.state.flashCard.answer}/></p>
-                                <div className="card-actions">
-                                    <p>Resource Label: <input type="text" value={this.state.flashCard.resourceName}/></p>
-                                    <p>Resource Link: <input type="text" value={this.state.flashCard.resourceLink} /></p>
-                                </div>                    
-                            </div> 
-                            <button onClick={this.Save}>Save</button>
+                        <div className="new-card-form">
+                            <div className="form-group">
+                                <div className="new-card-header">
+                                    <label htmlFor="eQuestion">Question:</label>  
+                                    {this.state.questionError && <span className="error">Question is required</span>}                                          
+                                </div>                            
+                                <textarea id="eQuestion" className='card-field' type="text" defaultValue={this.state.flashCard.question} />
+                            </div>
+
+                            <div className="form-group">
+                                <div className="new-card-header">
+                                    <label htmlFor="eAnswer">Answer:</label>  
+                                    {this.state.answerError && <span className="error">Answer is required</span>}                                          
+                                </div>
+                                <textarea id="eAnswer" className='card-field' type="text" defaultValue={this.state.flashCard.answer}/>
+                            </div>
+
+                            <div className="form-group">
+                                <div className="new-card-header">
+                                    <label htmlFor="eResLink">Resource Link:</label>                                          
+                                </div>
+                                <input id="eResLink" className='card-field' type="text" defaultValue={this.state.flashCard.resourceLink} />
+                            </div>
+
+                            <button id='new-btn' onClick={this.Save}>Save</button>
                         </div>
                     }
-                    
                 </div>
                 )
     }
--- src/main/java/com/cards/fullstack/controller/FlashCardController.java
+++ src/main/java/com/cards/fullstack/controller/FlashCardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +33,13 @@ public class FlashCardController {
 	public ResponseEntity<List<FlashCard>> getCards()
 	{
 		return new ResponseEntity<List<FlashCard>>(flashCardService.getAllFlashCards(), HttpStatus.OK);
+	}
+	
+	@PutMapping("/updateCard")		
+	public ResponseEntity<FlashCard> updateCard(@RequestBody FlashCard flashCard)
+	{
+		flashCardService.updateFlashCard(flashCard);
+		return new ResponseEntity<FlashCard>(flashCard, HttpStatus.OK); 
 	}
 	
 	@PostMapping("/addCard")		
